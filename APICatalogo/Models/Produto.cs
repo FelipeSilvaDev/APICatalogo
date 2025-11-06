ee9880aac38cb8f6c64f081add39bfe34d7b2aaa
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-11-06 13:24:33 -0300

    Definido o relacionamento entre as entidades

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,4 +1,4 @@
-﻿namespace APICatalogo.Models;  
+﻿namespace APICatalogo.Models;
 
 public class Produto
 {
@@ -9,4 +9,6 @@ public class Produto
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
 }
